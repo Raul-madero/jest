c4f99a85317876d8363fbbd47f3a5c33d184b4ad
Raul Madero <r.madero.ramirez@gmail.com>
2023-11-08 20:24:06 -0600

    Ultimos test

--- app.js
+++ app.js
@@ -2,5 +2,21 @@ const sum = (a, b) => {
     return a + b
 }
 console.log(sum(7, 3))
-
-module.exports = { sum };
+let oneEuroIs = {
+    "JPY" : 156.5,
+    "USD" : 1.07,
+    "GBP" : 0.87
+}
+const fromEuroToDollar = (euro) => {
+    let dollar = euro * 1.07
+    return dollar
+}
+const fromDollarToYen = (dollar) => {
+    let yen = (dollar / oneEuroIs.USD) * oneEuroIs.JPY;
+    return yen
+}
+const fromYenToPound = (yen) => {
+    let pound = (yen / oneEuroIs.JPY) * oneEuroIs.GBP;
+    return pound
+}
+module.exports = { sum, fromEuroToDollar, fromDollarToYen, fromYenToPound };
--- app.test.js
+++ app.test.js
@@ -1,5 +1,18 @@
-const {sum} = require('./app.js');
+const {sum, fromDollarToYen, fromYenToPound, fromEuroToDollar} = require('./app.js');
 test('adds 14 + 9 to equal 23', () => {
+
     let total = sum(14, 9);
     expect(total).toBe(23);
+});
+test('3.5 euro must be 3.745 dollars', () => {
+    let dollars = fromEuroToDollar(3.5);
+    expect(dollars).toBe(3.745);
+});
+test('3.745 dollars must be 547.75 yen', () => {
+    let yen = fromDollarToYen(3.745);
+    expect(yen).toBe(547.75);
+});
+test('547.75 yens must be 3.045 pounds', () => {
+    let pound = fromYenToPound(547.75);
+    expect(pound).toBe(3.045);
 });
